072d14f025d134df389c90116523b24f995b3e4a
amazee13 <amna.zafar1312@gmail.com>
2025-09-30 18:48:52 +0100

    Reverse and array

---
+++ src/ReverseArray.java
@@ -0,0 +1,11 @@
+public class ReverseArray {
+    public static void main(String[] args){
+
+        int a[] ={10,30,55,60,77,80};
+        for(int i=a.length-1;i>=0; i--){
+
+            System.out.println(a[i]);
+        }
+
+    }
+}
